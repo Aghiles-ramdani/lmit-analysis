f17e39e651521b800b062821859354e08ccae59c
andretomas <andretomas@gmail.com>
2012-10-18 16:51:39 +0100

    Algoritmo Completado

--- bruteforce.py
+++ bruteforce.py
@@ -1,28 +1,57 @@
 import utils
 import scipy
+import numpy
 
 # Inicializacao e importacao de dados
 
 mat_file = utils.read_mat("final.mat")
-s2 = mat_file["s2"][0,0]
+s2 = mat_file["s2"]
 probab = mat_file["probab"]
 Teste = mat_file["Teste"]
 maquinas = mat_file["maquinas"]
-x_teste = mat_file["x_teste"][:,0]
-n_maquinas = mat_file["n_maquinas"][0]
-pesos = mat_file["pesos"][0]
+x_teste = mat_file["x_teste"]
+n_maquinas = mat_file["n_maquinas"]
+pesos = mat_file["pesos"]
 
-l = scipy.shape(n_maquinas)[0]
+l = scipy.shape(pesos)[1]
 t = scipy.shape(x_teste)[0]
 optimo = scipy.zeros([t,l,5])
 
 # Minimizacao
-for n in range(0,t-1):
+for n in range(0,1):
     
     a = 0
-    permut = scipy.zeros([t])
+    b = 0
+    permut = scipy.zeros(l)
     best =[float('Inf')]*5
-    print n
     
     if x_teste[n] == 0:
         continue
+    
+    while(True):
+        if x_teste[n]-numpy.dot(pesos,permut) != 0:
+            [permut,a]=utils.prox(n_maquinas[0],0,permut)
+            continue
+        
+        custo = 1/(2*s2)*(x_teste[n] - numpy.dot(pesos, permut))**2 - numpy.dot(permut,numpy.log(probab[n,:])) - numpy.dot((1-permut,),numpy.log(1-probab[n,:]))
+        
+        for i in xrange(5):
+            if custo < best[i]:
+                if i == 5:
+                    best[i] = custo
+                    optimo[n,:,i] = permut
+                    break
+                
+                for m in range(0,4-i):
+                    best[i] = custo
+                    optimo[n,:,i]
+                
+                best[i] = custo
+                optimo[n,:,i] = permut
+                break
+        
+        [permut, a] = utils.prox(n_maquinas[0], 0, permut)
+        
+        if a == 1:
+            break
+    
