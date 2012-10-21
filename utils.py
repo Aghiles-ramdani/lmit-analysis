056e10bfef99162ba137c74b4d7b871cb57bcd34
andretomas <andretomas@gmail.com>
2012-10-22 00:02:58 +0100

    utils com + 2 funções, bug esquisito em proxm

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import scipy.io
+import numpy
 
 def read_mat(filename):
     mat_file = scipy.io.loadmat(filename)
@@ -32,3 +33,32 @@ def prox(n_maquinas, maquina, permut):
     permut[pos] = 1
     
     return permut, a
+
+def init_markov(permut, n_maquinas, linha):
+    c = numpy.shape(permut)[1]
+    
+    b = numpy.shape(n_maquinas)[0]
+    
+    permut[linha,:] = numpy.zeros([1,c])
+    
+    for m in range(b):
+        pos = numpy.sum(n_maquinas[0:m])
+        permut [linha,pos] = 1
+    return permut
+    
+
+def proxm(n_maquinas, permut, slot, nslots):
+    
+    [permut[slot,:],b] = prox(n_maquinas,0, permut[slot,:])
+    a = 0
+    
+    if b == 1:
+        if slot == nslots-1:
+            a = 1
+            return permut,a
+        
+        permut = init_markov(permut, n_maquinas, slot)
+        [permut,a] = proxm(n_maquinas, permut, slot+1, nslots)
+        return permut,a
+    
+    return permut,a
